e1269aeb288830c4bcc1614ed166eb93382ad3b3
Arpit Singh <arpitlibono57@gmail.com>
2025-11-02 00:17:24 +0530

    added animation perfectly adjust the logo

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,24 +4,24 @@ import Splineanimation from './splineanimation';
 
 const HeroSection = () => {
   return (
-    <div className='flex justify-between items-center '>
-     
-    <div className='h-screen w-[60%] bg-gradient-tr-bl  from-black via-yellow-300 to-black flex flex-col justify-center items-start font-bold p-10 gap-10'>
-         <div className='bg-gradient-to-r from-red-400 to-blue-400 flex justify-center text-center  font-extrabold text-7xl bg-clip-text text-transparent p-4   '> Hey, what’s up? I’m Arpit</div>
-         <div className='bg-gradient-to-r from-red-400 to-purple-400 flex justify-center  text-center font-extrabold text-5xl bg-clip-text text-transparent p-4 '>
-           <ReactTyped 
-          strings= {[
-            "Developer by passion, creator by choice." , 
-            "I build awesome things for the web."
-          ]}
-        typeSpeed={20}
-        backSpeed={60}
-        loop
-         /> </div>
+    <div className='flex justify-between items-center bg-gradient-to-tr from-black via-gray-900  to-yellow-950 '>
+      
+    <div className='h-screen w-[60%] bg-transparent flex flex-col justify-center items-start font-bold p-10 gap-10'>
+         <div className='bg-gradient-to-r from-red-400 to-blue-400 flex justify-center text-center font-extrabold text-6xl bg-clip-text text-transparent p-4 '> Hey, what’s up? I’m Arpit</div>
+         <div className='bg-gradient-to-r from-red-400 to-purple-400 flex justify-center text-center font-extrabold text-4xl bg-clip-text text-transparent p-4 '>
+            <ReactTyped 
+           strings= {[
+             "Developer by passion, creator by choice." , 
+             "I build awesome things for the web."
+           ]}
+         typeSpeed={20}
+         backSpeed={60}
+         loop
+           /> </div>
     </div>
-     <div className='h-screen w-[40%] flex justify-center items-center bg-amber-100 ' >
-     <Splineanimation/>
-     </div>
+      <div className='h-screen w-[40%] flex justify-center items-center overflow-hidden ' >
+      <Splineanimation/>
+      </div>
     </div>
   )
 }
