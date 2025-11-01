4424975d0cfd79004b2c19504c8eb4a6d364d181
Arpit Singh <arpitlibono57@gmail.com>
2025-11-01 23:56:35 +0530

    animation added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { ReactTyped } from "react-typed";
+import Splineanimation from './splineanimation';
 
 const HeroSection = () => {
   return (
-    <div className='h-screen w-full bg-[#222] flex flex-col justify-center items-start font-bold p-10 gap-10'>
+    <div className='flex justify-between items-center '>
+     
+    <div className='h-screen w-[60%] bg-gradient-tr-bl  from-black via-yellow-300 to-black flex flex-col justify-center items-start font-bold p-10 gap-10'>
          <div className='bg-gradient-to-r from-red-400 to-blue-400 flex justify-center text-center  font-extrabold text-7xl bg-clip-text text-transparent p-4   '> Hey, what’s up? I’m Arpit</div>
          <div className='bg-gradient-to-r from-red-400 to-purple-400 flex justify-center  text-center font-extrabold text-5xl bg-clip-text text-transparent p-4 '>
            <ReactTyped 
@@ -15,6 +18,10 @@ const HeroSection = () => {
         backSpeed={60}
         loop
          /> </div>
+    </div>
+     <div className='h-screen w-[40%] flex justify-center items-center bg-amber-100 ' >
+     <Splineanimation/>
+     </div>
     </div>
   )
 }
---
+++ src/components/Splineanimation.jsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+import "./Splineanimation.scss"
+export default function Splineanimation() {
+  return (
+    <div className='spline-scene'>
+    <Spline   scene="https://prod.spline.design/fztIi05bcJDyhOXR/scene.splinecode" />
+    </div>
+  );
+}
